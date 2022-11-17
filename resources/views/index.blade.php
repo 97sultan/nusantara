4d55460134c500fea3f263ae0f60efd822cca270
97sultan <57515238+97sultan@users.noreply.github.com>
2022-11-17 12:10:00 +0700

    u5

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 
-<div style="background-image: url({{ asset('img/alphardcover.png') }}); height:100vh;background-repeat: no-repeat; background-size: 100%;" class="d-none d-md-block">
+<div style="background-image: url({{ asset('img/home.jpg') }}); height:100vh;background-repeat: no-repeat; background-size: 100%;" class="d-none d-md-block">
   <div class="text-center">
     <h1 class="py-md-3 py-lg-5 text-dark font-weight-bold">Nusantara Armada</h1>
     <h1 class="text-center text-white border d-inline px-md-2 py-lg-3 rounded-pill bg-primary">Solusi Layanan Sewa Mobil</h1>
@@ -52,7 +52,7 @@
 <!-- FOR MOBILE -->
 <div class="d-md-none">
     
-    <img src="{{ asset('img/alphardcover.png') }}" class="img-fluid">
+    <img src="{{ asset('img/home.jpg') }}" class="img-fluid">
     <hr>
     <div class="container text-center">
         <h1 class="text-secondary font-weight-bold h3">Nusantara Armada</h1>
