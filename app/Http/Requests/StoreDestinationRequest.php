00c89708e461d296aec1fc4ad9a72ac96251e8dc
97sultan <57515238+97sultan@users.noreply.github.com>
2022-11-24 09:50:05 +0700

    validation unique destination

--- app/Http/Requests/StoreDestinationRequest.php
+++ app/Http/Requests/StoreDestinationRequest.php
@@ -24,7 +24,7 @@ class StoreDestinationRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required',
+            'name' => 'required|unique:destinations',
             'image' => 'required|image',
             'description' => 'required',
         ];
